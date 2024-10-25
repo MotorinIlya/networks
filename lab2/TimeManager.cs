c8d613593897f261fbcf05dca358f5be97816fa7
Ilya Motorin <motorin_0404@mail.ru>
2024-10-25 14:58:56 +0700

    change count time

--- lab2/TimeManager.cs
+++ lab2/TimeManager.cs
@@ -9,59 +9,89 @@ namespace SendingFiles
         DateTime waitTime;
         int wait;
         Stopwatch sw;
-        long freq = Stopwatch.Frequency;
+        static long freq = Stopwatch.Frequency;
+        const int countNumbersAfterDot = 3;
         long time;
-        long count_all_bytes;
-        long count_bytes_now;
-        long all_bytes;
-        public TimeManager(int wait, string nameFile, long all_bytes)
+        long countAllBytes;
+        long countBytesNow;
+        long countTicksNow;
+        long allBytes;
+        bool isEnd;
+        public TimeManager(int wait, string nameFile, long allBytes)
         {
             this.wait = wait;
             this.waitTime = DateTime.Now;
-            this.all_bytes = all_bytes;
+            this.allBytes = allBytes;
             this.nameFile = nameFile;
             this.sw = new Stopwatch();
             this.time = 0;
-            count_all_bytes = 0;
-            count_bytes_now = 0;
+            countAllBytes = 0;
+            countBytesNow = 0;
+            countTicksNow = 0;
+            isEnd = false;
         }
 
-        public void Start() { sw.Start(); }
-        public void Restart() { sw.Restart(); }
+        public void Start() 
+        { 
+            sw.Start(); 
+        }
+        public void Restart() 
+        { 
+            sw.Restart(); 
+        }
 
         public void Stop()
         {
             sw.Stop();
             time += sw.ElapsedTicks;
+            countTicksNow += sw.ElapsedTicks;
         }
 
         public void AddBytes(long bytes)
         {
-            count_all_bytes += bytes;
-            count_bytes_now = bytes;
+            countAllBytes += bytes;
+            countBytesNow += bytes;
+        }
+
+        private void PrintSpeedInformation(string typeSpeed, double bytes, double ticks)
+        {
+            double speed = Math.Round(
+                                    bytes * freq / ticks / 1_000_000,
+                                    countNumbersAfterDot);
+            Console.WriteLine(nameFile + $": {typeSpeed} speed is " + speed.ToString() + " MB/s");
         }
 
         public void PrintSpeed(bool periodic) 
         {
 
-            if ((DateTime.Now.CompareTo(waitTime) >= 0) || !periodic) 
+            if ((DateTime.Now.CompareTo(waitTime) >= 0) || !periodic && !isEnd) 
             {
                 waitTime = waitTime.AddSeconds(wait);
 
-                double moment_speed = ((double)count_bytes_now * (double)freq / (double)sw.ElapsedTicks) / 1_000_000;
-                Console.WriteLine(nameFile + ": moment speed is " + moment_speed.ToString() + " MB/s");
+                PrintSpeedInformation("moment", countBytesNow, countTicksNow);
 
-                double average_speed = ((double)count_all_bytes * (double)freq / (double)time) / 1_000_000;
-                Console.WriteLine(nameFile + ": average speed is " + average_speed + " MB/s");
+                PrintSpeedInformation("average", countAllBytes, time);
 
-                Console.WriteLine(nameFile + $": is {(count_all_bytes * 100) / all_bytes}% complete");
+                Console.WriteLine(nameFile + $": is {(countAllBytes * 100) / allBytes}% complete");
                 Console.WriteLine(nameFile + $": spend {time / freq} seconds");
+                
+                IsEnd();
+                RestartCountNow();
             }
         }
 
-        public void PrintInformation(string info)
+        private void RestartCountNow()
         {
-            Console.WriteLine(nameFile + ": " + info);
+            countBytesNow = 0;
+            countTicksNow = 0;
+        }
+
+        private void IsEnd()
+        {
+            if (countAllBytes == allBytes)
+            {
+                isEnd = true;
+            }
         }
     }
 }
